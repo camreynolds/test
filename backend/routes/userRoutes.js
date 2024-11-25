9a0eae49ec0a59d1a6bc41e0639f8c8079ab6306
carlos <camr.personal@gmail.com>
2024-11-25 13:50:59 -0500

    erase at all.

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const {signup,login} = require("../controllers/userControllers")
-const express = require("express")
-const userRoutes= express.Router()
-
-userRoutes.post("/signup", signup)
-userRoutes.post("/login", login)
-
-module.exports = userRoutes
