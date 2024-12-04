fd0327d26a4f62631ff7d0d79573f44844a97225
carlos <camr.personal@gmail.com>
2024-12-04 15:22:05 -0500

    workouts backend done.

--- backend/controllers/workoutsControllers.js
+++ backend/controllers/workoutsControllers.js
@@ -1,26 +1,96 @@
+const mongoose = require("mongoose")
+const Workout = require("../model/workoutsSchema")
+
 // Get all the workouts
-const getAllWorkouts = (req,res) =>{
-  res.status(200).json({mssg: "get all the workouts"})
+const getAllWorkouts = async (req,res) =>{
+  try {
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+    res.status(200).json(workouts)
+  } catch (error) {
+    res.status(400).json({error: "workouts couldn't be loaded."})
+  }
 }
 
 // Get a single workout
-const getSingleWorkout = (req,res)=>{
-  res.status(200).json({mssg: "get a single workout."})
+const getSingleWorkout = async (req,res)=>{
+  const {_id} = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(_id)){
+    return res.status(400).json({error: "this is not a valid id."})
+  }
+
+  const workout = await Workout.findById({_id})
+
+  if(!workout){
+    return res.status(400).json({error: "workout doesn't exist."})
+  }
+
+  res.status(200).json(workout)
 }
 
 // Create a single workout
-const createWorkout = (req,res)=>{
-  res.status(200).json({mssg: "create a single workout."})
+const createWorkout = async (req,res)=>{
+  const {title,load,reps} = req.body
+  const emptyFields = []
+
+  if(!title){
+    emptyFields.push("title")
+  }
+
+  if(!load){
+    emptyFields.push("load")
+  }
+
+  if(!reps){
+    emptyFields.push("reps")
+  }
+
+  if(emptyFields.length > 0){
+    return res.status(400).json({error: "all the fields must be filled in.", emptyFields})
+  }
+
+  try {
+    const workouts = await Workout.create({title,load,reps})
+    res.status(200).json(workouts)
+  } catch (error) {
+    res.status(400).json({error: "workout couldn't be created."})
+  }
 }
 
 // Update a single workout
-const updateWorkout = (req,res)=>{
-  res.status(200).json({mssg: "update a single workout."})
+const updateWorkout = async (req,res)=>{
+  const {_id} = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(_id)){
+    return res.status(400).json({error: "this is not a valid id."}) 
+  }
+
+  const workout = await Workout.findByIdAndUpdate({_id},{
+    ...req.body
+  })
+
+  if(!workout){
+    return res.status(400).json({error: "workout doesn't exist."})
+  }
+
+  res.status(200).json(workout)
 }
 
 // Delete a single workout
-const deleteWorkout = (req,res)=>{
-  res.status(200).json({mssg: "delete a single workout."})
+const deleteWorkout = async (req,res)=>{
+  const {_id} = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(_id)){
+    return res.status(400).json({error: "this is not a valid id."})
+  }
+
+  const workout = await Workout.findByIdAndDelete({_id})
+
+  if(!workout){
+    return res.status(400).json({error: "workout couldn't be deleted."})
+  }
+
+  res.status(200).json(workout)
 }
 
 module.exports = {getAllWorkouts,getSingleWorkout,createWorkout,updateWorkout,deleteWorkout}
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
-require("dotenv").config()
 const workoutsRouter = require("./routes/workoutsRoutes")
+const mongoose = require("mongoose")
 
 // middleware
 app.use(express.json())
+app.use((req,res,next)=>{
+  console.log(req.path, req.method)
+  next()
+})
 
 // endpoints
 app.use("/api/workouts", workoutsRouter)
 
-// server running
-app.listen(process.env.PORT, ()=>{
-  console.log("server running at port:",process.env.PORT)
-})
+// server & database running
+mongoose.connect(process.env.MONGO_URI)
+  .then(
+    app.listen(process.env.PORT, ()=>{
+      console.log("server & database running at port:",process.env.PORT)
+    })
+  )
+  .catch(error =>{
+    console.log(error)
+  })
