8802244cc294cc4fa458d7261dbe4c5b8e48207d
carlos <camr.personal@gmail.com>
2024-12-04 12:54:55 -0500

    All files deleted.

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const userRoutes = express.Router()
-const {userSignup,userLogin} = require("../controllers/userControllers")
-
-userRoutes.post("/signup", userSignup)
-userRoutes.post("/login", userLogin)
-
-module.exports = userRoutes
